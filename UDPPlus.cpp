40f98cf8265efba2293427ae2ab76c3b79cbf3ca
Asad Saeed <asad.a.saeed@gmail.com>
2010-04-17 14:48:35 -0400

    some more changes

--- UDPPlus.cpp
+++ UDPPlus.cpp
@@ -6,19 +6,62 @@
  */
 
 #include "UDPPlus.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
 
 UDPPlus::UDPPlus() {
-  ackedLocation = 0;
-  lastSentLocation = 0;
-  packetBufferSize = 1024;
-  packetBuffer = new Packet*[packetBufferSize];
+
 }
 
 UDPPlus::~UDPPlus() {
-  for (unsigned i=0; i<packetBufferSize; i++) {
-    if (packetBuffer[i] != NULL) {
-      delete packetBuffer[i];
+  for (unsigned i=0; i<inBufferSize; i++) {
+    if (inBuffer[i] != NULL) {
+      delete inBuffer[i];
     }
   }
-  delete packetBuffer;
+  for (unsigned i=0; i<outBufferSize; i++) {
+    if (outBuffer[i] != NULL) {
+      delete outBuffer[i];
+    }
+  }
+  delete inBuffer;
+  delete outBuffer;
+}
+
+void UDPPlus::init() {
+  inBufferSize = 1024;
+  outBufferSize = 1024;
+  inBufferBegin = 0;
+  inBufferEnd = 0;
+  outBufferBegin = 0;
+  outBufferEnd = 0;
+
+  sock = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sock == -1) {
+    fprintf(stderr, "Sorry, could not create socket");
+    exit(0);
+  }
+}
+
+void UDPPlus::connect() {
+  inBuffer = new Packet*[inBufferSize];
+  outBuffer = new Packet*[outBufferSize];
+  // build connection information
+  uint16_t randomValue = rand() % 65536;
+  Packet *current = new Packet(Packet::SEQ | Packet::SYN, randomValue);
+  outBuffer[outBufferEnd++] = current;
+}
+
+void UDPPlus::close() {
+  Packet *current = new Packet(Packet::SEQ | Packet::FIN, lastSeqNum++);
+  outBuffer[outBufferEnd++] = current;
+  // close connection
+}
+
+void UDPPlus::recieve() {
+
+
 }
--- UDPPlus.h
+++ UDPPlus.h
@@ -9,24 +9,43 @@
 #define UDPPLUS_H_
 
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdint.h>
 #include "Packet.h"
+
+enum State { LISTEN, SYN_SENT, SYN_RECIEVED, ESTABLISHED, FIN_WAIT1, FIN_WAIT2, CLOSE_WAIT, CLOSING, LAST_ACK };
 
 class UDPPlus {
 public:
   UDPPlus();
   virtual ~UDPPlus();
 
+  void init();
+  void connect();
+  void close();
+  void recieve();
+
+
   ssize_t recv(int s, void *buf, size_t len, int flags);
   int bind();
 
 private:
-  Packet **packetBuffer; // for array of pointers
-  unsigned packetBufferSize;
-  int ackedLocation;
-  int lastSentLocation;
+
+  int sock;
+  State currentState;
+  Packet **inBuffer; // for array of pointers
+  Packet **outBuffer;
+  unsigned inBufferSize;
+  unsigned outBufferSize;
+  uint16_t inBufferBegin;
+  uint16_t inBufferEnd;
+  uint16_t outBufferBegin;
+  uint16_t outBufferEnd;
+  uint16_t lastAckNum;
+  uint16_t lastSeqNum;
+
 };
 
 #endif /* UDPPLUS_H_ */
